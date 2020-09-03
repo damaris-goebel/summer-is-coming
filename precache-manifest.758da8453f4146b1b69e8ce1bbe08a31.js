3a41ebb044ed2143ce1027282581997b6ab51bfc
Damaris Goebel <damaris.goebel@gmail.com>
2020-09-03 09:21:54 +0200

    Updates

---
+++ precache-manifest.758da8453f4146b1b69e8ce1bbe08a31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5978a3a4ae27c33ba24c5a3407849046",
+    "url": "/summer-is-coming/index.html"
+  },
+  {
+    "revision": "b37ce0602bbc3e9d8fce",
+    "url": "/summer-is-coming/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "34fb7d519dd03774fcbc",
+    "url": "/summer-is-coming/static/css/main.e3c3efa3.chunk.css"
+  },
+  {
+    "revision": "b37ce0602bbc3e9d8fce",
+    "url": "/summer-is-coming/static/js/2.272eebdd.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/summer-is-coming/static/js/2.272eebdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34fb7d519dd03774fcbc",
+    "url": "/summer-is-coming/static/js/main.32333913.chunk.js"
+  },
+  {
+    "revision": "68bf293fe19e9d78497c",
+    "url": "/summer-is-coming/static/js/runtime-main.6fff84dc.js"
+  }
+]);
